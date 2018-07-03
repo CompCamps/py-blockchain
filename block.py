65284a0773ce3abb8e43be1eb3ab387deafdea5e
Taylor Petrychyn <tpetrychyn@gmail.com>
2018-07-03 14:33:26 -0600

    step-4

--- block.py
+++ block.py
@@ -3,19 +3,17 @@ import hashlib as hashlib
 class Block:
     def __init__(self, index, nonce, data, previousHash, hash=None):
         self.index = index
-
-        # TODO: Add the pieces of a block
-        # TODO: 'self.hash' should be a the SHA256 of the block
-
+        self.nonce = nonce
+        self.data = data
+        self.previousHash = previousHash
+        self.hash = self.hashBlock()
 
     # Takes all the block fields and combines them into one big string
     # then hashes that string
     def hashBlock(self):
         hash = hashlib.sha256((
             str(self.index) +
-
-            # TODO: Include the nonce in the hash calculation
-            
+            str(self.nonce) +
             str(self.data) + 
             str(self.previousHash)).encode('utf-8')).hexdigest()
         return hash
@@ -25,3 +23,15 @@ class Block:
         if self.hash[:3] != "000":
             return False
         return True
+
+    # Prints the block to the screen
+    def print(self):
+        print("Block #: " + str(self.index))
+        print("Nonce: " + str(self.nonce))
+        print("Data: " + str(self.data))
+        print("Previous Hash: " + str(self.previousHash))
+        print("Hash: " + str(self.hash))
+
+    # Returns the block that comes after this block
+    def nextBlock(self, nonce, data):
+        return Block(self.index + 1, nonce, data, self.hash)
--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from block import Block
 
-nonce = 0
-block = Block(0, nonce, "hello world", 0)
+blockchain = []
 
+# Genesis Block
+blockchain.append(Block(0, 0, "hello world", 0, 0))
+
+
+nonce = 0
+data = "new block!"
+block = #TODO: get the last block in the blockchain, and call nextBlock on it
 while not block.validate():
     nonce = nonce + 1
-    block = Block(0, nonce, "hello world", 0)
+    block = #TODO: get the last block in the blockchain, and call nextBlock on it
 
-print(str(block.hash))
+# TODO: Append the newly mined block to the blockchain
+# TODO: Repeat this infinitely
+
+block.print()
